79818d7ea4224542cc8aa707121e4a02ef62b2bd
zeina <catsdisownedz@gmail.com>
2025-05-19 03:04:09 +0300

    huffman finally working

--- huffmancompressiontool/huffman/visualizer.py
+++ huffmancompressiontool/huffman/visualizer.py
@@ -19,10 +19,14 @@ class HuffmanVisualizer:
         self.G = nx.DiGraph()
         self.pos = {}
         self.labels = {}
+        self.hidden_nodes = set()
+        self.visible_edges = set()
+
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.canvas_frame)
         self.canvas_widget = self.canvas.get_tk_widget()
         self.canvas_widget.pack(fill="both", expand=True)
-        self._build_graph(self.root_node)  # Layout remains fixed from start
+
+        self._build_graph(self.root_node)
 
     def _build_graph(self, node, x=0, y=0, layer=2.0):
         if node is None:
@@ -31,15 +35,21 @@ class HuffmanVisualizer:
         char_label = node.character if node.character else self._get_combined_label(node)
         self.labels[node_id] = f"{char_label}\n{node.freq}"
         self.pos[node_id] = (x, -y)
+        self.hidden_nodes.add(node_id)
+
         if node.character:
             self.node_map[node.character] = node_id
+
         if node.left:
-            self.G.add_edge(node_id, id(node.left))
-            self.arrow_labels[(node_id, id(node.left))] = '0'
+            edge = (node_id, id(node.left))
+            self.G.add_edge(*edge)
+            self.arrow_labels[edge] = '0'
             self._build_graph(node.left, x - 1 / layer, y + 1, layer * 2.0)
+
         if node.right:
-            self.G.add_edge(node_id, id(node.right))
-            self.arrow_labels[(node_id, id(node.right))] = '1'
+            edge = (node_id, id(node.right))
+            self.G.add_edge(*edge)
+            self.arrow_labels[edge] = '1'
             self._build_graph(node.right, x + 1 / layer, y + 1, layer * 2.0)
 
     def _get_combined_label(self, node):
@@ -53,40 +63,94 @@ class HuffmanVisualizer:
 
     def _draw(self):
         self.ax.clear()
+
+        visible_nodes = [n for n in self.G.nodes() if n not in self.hidden_nodes]
+
+        # Draw nodes
         node_colors = []
         for node in self.G.nodes():
-            if node == self.current_node:
-                node_colors.append("#FF69B4")
+            if node not in visible_nodes:
+                node_colors.append((0, 0, 0, 0))  # transparent
+            elif node == self.current_node:
+                node_colors.append("#FF69B4")  # pink
             elif node in self.visited:
-                node_colors.append("grey")
+                node_colors.append("lightgrey")
             else:
                 node_colors.append("#FFE4B5")
 
-        nx.draw(self.G, self.pos, with_labels=False, arrows=False, node_size=self.node_size,
-                node_color=node_colors, ax=self.ax)
-        nx.draw_networkx_labels(self.G, self.pos, self.labels, ax=self.ax, font_size=self.font_size)
-        nx.draw_networkx_edge_labels(self.G, self.pos, edge_labels=self.arrow_labels, font_color='black', ax=self.ax)
+        nx.draw_networkx_nodes(
+            self.G, self.pos, nodelist=self.G.nodes(), node_color=node_colors,
+            node_size=self.node_size, ax=self.ax
+        )
+
+        # Draw labels for visible nodes
+        visible_labels = {n: self.labels[n] for n in visible_nodes}
+        nx.draw_networkx_labels(
+            self.G, self.pos, visible_labels, ax=self.ax, font_size=self.font_size
+        )
+
+        # Draw only visible edges and their labels
+        visible_edges = list(self.visible_edges)
+
+        nx.draw_networkx_edges(
+            self.G,
+            self.pos,
+            edgelist=visible_edges,
+            ax=self.ax,
+            edge_color="black",
+            arrows=False
+        )
+
+        visible_edge_labels = {
+            e: self.arrow_labels[e] for e in visible_edges if e in self.arrow_labels
+        }
+
+        nx.draw_networkx_edge_labels(
+            self.G,
+            self.pos,
+            edge_labels=visible_edge_labels,
+            font_color="black",
+            ax=self.ax,
+            font_size=self.font_size - 2  # smaller edge label font
+        )
+
         self.ax.set_axis_off()
         self.canvas.draw()
 
     def animate_build(self):
-        def postorder(node):
-            if node is None:
+        def gather_merge_steps(node):
+            if node is None or node.character is not None:
                 return []
-            return postorder(node.left) + postorder(node.right) + [node]
+            steps = []
+            left_id = id(node.left)
+            right_id = id(node.right)
+            parent_id = id(node)
+            steps += gather_merge_steps(node.left)
+            steps += gather_merge_steps(node.right)
+            return steps + [[left_id, right_id, parent_id]]
 
-        nodes_in_order = postorder(self.root_node)
+        merge_steps = gather_merge_steps(self.root_node)
         self.visited.clear()
-        for node in nodes_in_order:
-            node_id = id(node)
-            self.current_node = node_id
-            self.visited.add(node_id)
+        self.current_node = None
+
+        for step in merge_steps:
+            for node_id in step:
+                self.hidden_nodes.discard(node_id)
+                self.current_node = node_id
+
+                # Reveal incoming edge to this node
+                for parent, child in self.G.edges():
+                    if child == node_id:
+                        self.visible_edges.add((parent, child))
+
             self._draw()
             self.canvas_frame.update()
             time.sleep(self.speed_ms / 1000)
 
-        self.current_node = None
-        self._draw()
+            for node_id in step:
+                self.visited.add(node_id)
+            self.current_node = None
+            self._draw()
 
     def highlight_encoding_path(self, char):
         code = self.huffman_codes.get(char)
